ce0755d5be001a0df0c4a7aa29c4fd745cdcb03a
Ghopp3r <Ghopp3r@users.noreply.github.com>
2026-06-22 22:55:21 +0300

    Rename kernel log tag

--- driver/src/log.h
+++ driver/src/log.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-// logging wrappers; "[driver] " prefix used so dmesg lines are uniformly tagged.
+// logging wrappers; DRV_LOG_TAG prefix used so dmesg lines are uniformly tagged.
 #ifndef DRIVER_LOG_H
 #define DRIVER_LOG_H
 
@@ -7,16 +7,16 @@
 #include <linux/printk.h>
 
 #ifndef DRV_LOG_TAG
-#define DRV_LOG_TAG "[driver]"
+#define DRV_LOG_TAG "[memory-driver]"
 #endif
 
-#define pr_drv(fmt, ...) printk(KERN_INFO "[driver] " fmt, ##__VA_ARGS__)
-#define pr_drv_err(fmt, ...) printk(KERN_ERR "[driver] " fmt, ##__VA_ARGS__)
-#define pr_drv_warn(fmt, ...) printk(KERN_WARNING "[driver] " fmt, ##__VA_ARGS__)
-#define pr_drv_notice(fmt, ...) printk(KERN_NOTICE "[driver] " fmt, ##__VA_ARGS__)
+#define pr_drv(fmt, ...) printk(KERN_INFO DRV_LOG_TAG " " fmt, ##__VA_ARGS__)
+#define pr_drv_err(fmt, ...) printk(KERN_ERR DRV_LOG_TAG " " fmt, ##__VA_ARGS__)
+#define pr_drv_warn(fmt, ...) printk(KERN_WARNING DRV_LOG_TAG " " fmt, ##__VA_ARGS__)
+#define pr_drv_notice(fmt, ...) printk(KERN_NOTICE DRV_LOG_TAG " " fmt, ##__VA_ARGS__)
 
 #ifdef CONFIG_DRIVER_VERBOSE_DEBUG
-#define pr_drv_dbg(fmt, ...) printk(KERN_DEBUG "[driver] " fmt, ##__VA_ARGS__)
+#define pr_drv_dbg(fmt, ...) printk(KERN_DEBUG DRV_LOG_TAG " " fmt, ##__VA_ARGS__)
 #else
 #define pr_drv_dbg(fmt, ...) do { } while (0)
 #endif
